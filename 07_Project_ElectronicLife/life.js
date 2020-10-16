e37df239b90b930882be7c600f26af8b44f7bfca
paalso <40a.pvl@gmail.com>
2020-10-16 22:16:11 +0300

    Representing space

--- 07_Project_ElectronicLife/life.js
+++ 07_Project_ElectronicLife/life.js
@@ -1,16 +1,49 @@
 const plan =
-    ["############################",
-        "#      #    #      o      ##",
-        "#                          #",
-        "#          #####           #",
-        "##         #   #    ##     #",
-        "###           ##     #     #",
-        "#           ###      #     #",
-        "#   ####                   #",
-        "#   ##       o             #",
-        "# o  #         o       ### #",
-        "#    #                     #",
-        "############################"];
+  ["############################",
+    "#      #    #      o      ##",
+    "#                          #",
+    "#          #####           #",
+    "##         #   #    ##     #",
+    "###           ##     #     #",
+    "#           ###      #     #",
+    "#   ####                   #",
+    "#   ##       o             #",
+    "# o  #         o       ### #",
+    "#    #                     #",
+    "############################"];
 
 
-        Мы используем простой тип Vector (из упражнений к предыдущей главе) для представления этих пар координат.
+// Используем простой тип Vector для представления этих пар координат.
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+Vector.prototype.plus = function (other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+};
+
+// Объявляем объект Grid (сетка) с основными методами:
+function Grid(width, height) {
+  this.space = new Array(width * height);
+  this.width = width;
+  this.height = height;
+}
+
+Grid.prototype.isInside = function(vector) {
+  const [x, y] = [vector.x, vector.y];
+  return 0 <= x && x < this.width && 0 <= y && y < this.height;
+};
+
+Grid.prototype.get = function(vector) {
+  return this.space[vector.y * this.width + vector.x];
+};
+
+Grid.prototype.set = function(vector, value) {
+  this.space[vector.y * this.width + vector.x] = value;
+};
+
+// Test
+var grid = new Grid(5, 4);
+console.log(grid.get(new Vector(1, 1)));  // → undefined
+grid.set(new Vector(1, 1), "X");
+console.log(grid.get(new Vector(1, 1)));  // → X
